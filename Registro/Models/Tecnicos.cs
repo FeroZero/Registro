ab87c09e7c786abb469e00998c43ff0b7f928693
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-01-14 20:16:23 -0500

    Cree el modelo de tecnicos.

---
+++ Registro/Models/Tecnicos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registro.Models
+{
+	public class Tecnicos
+	{
+		[Key]
+		public int TecnicoId { get; set; }
+
+		[Required(ErrorMessage = "Este Campo es Requerido.")]
+		[RegularExpression(@"^(a-zA-Z\s)+$",ErrorMessage = "Solo Caracteres Alfabeticos.")]
+		public string Nombres { get; set; }
+
+		[Required(ErrorMessage = "Este Campo es Requerido.")]
+		[Range(1, double.MaxValue, ErrorMessage = "Solo Caracteres Numericos.")]
+		public decimal SueldoHora { get; set; }
+	}
+}
