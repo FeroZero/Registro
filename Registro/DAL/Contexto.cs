3f16f95986729eab48a2a6567ec30f2651852172
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-01-14 20:21:02 -0500

    Cree el contexto con su dbset de Tecnicos.

---
+++ Registro/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro.Models;
+
+namespace Registro.DAL
+{
+	public class Contexto : DbContext
+	{
+		public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+		public DbSet<Tecnicos> Tecnicos { get; set; }
+	}
+}
