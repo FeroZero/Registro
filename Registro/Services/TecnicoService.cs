da14c022c2e72ce2b885a4a1b852eb1489fb5b0e
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-01-14 20:23:06 -0500

    Cree el servicio de Tecnicos con todas sus funciones.

---
+++ Registro/Services/TecnicoService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro.DAL;
+using Registro.Models;
+using System.Linq.Expressions;
+
+namespace Registro.Services
+{
+	public class TecnicoService(IDbContextFactory<Contexto> DbFactory)
+	{
+		public async Task<bool> Guardar(Tecnicos tecnico)
+		{
+			if (!await Existe(tecnico.TecnicoId))
+				return await Insertar(tecnico);
+			else
+				return await Modificar(tecnico);
+		}
+
+		private async Task<bool> Modificar(Tecnicos tecnico)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			contexto.Update(tecnico);
+			return await contexto.SaveChangesAsync() > 0;
+		}
+
+		private async Task<bool> Insertar(Tecnicos tecnico)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			contexto.Tecnicos.Add(tecnico);
+			return await contexto.SaveChangesAsync() > 0;
+		}
+
+		private async Task<bool> Existe(int id)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			return await contexto.Tecnicos
+				.AnyAsync(p => p.TecnicoId == id);
+		}
+
+		public async Task<bool> Eliminar(int id)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			return await contexto.Tecnicos
+				.Where(p => p.TecnicoId == id)
+				.ExecuteDeleteAsync() > 0;
+		}
+
+		public async Task<Tecnicos?> Buscar(int id)
+		{
+			await using var contexo = await DbFactory.CreateDbContextAsync();
+			return await contexo.Tecnicos
+				.FirstOrDefaultAsync(p => p.TecnicoId == id);
+		}
+
+		public async Task<List<Tecnicos>> Listar(Expression<Func<Tecnicos, bool>> criterio)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			return await contexto.Tecnicos
+				.Where(criterio)
+				.ToListAsync();
+		}
+
+	}
+}
