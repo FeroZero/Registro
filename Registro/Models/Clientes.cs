f6107f94adc95c79f05a877e2de19fa762571a15
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-01-25 02:51:06 -0400

    Modelo Clientes Regulado.

--- Registro/Models/Clientes.cs
+++ Registro/Models/Clientes.cs
@@ -9,14 +9,24 @@ namespace Registro.Models
 		[Key]
 		public int ClienteId { get; set; }
 
+		[Required(ErrorMessage = "Este Campo es Requerido.")]
 		public DateTime FechaIngreso { get; set; } = DateTime.Now;
 
+		[Required(ErrorMessage = "Este Campo es Requerido.")]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo Caracteres Alfabeticos.")]
 		public string Nombres { get; set; }
 
+		[Required(ErrorMessage = "Este Campo es Requerido.")]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo Caracteres Alfabeticos.")]
 		public string Direccion { get; set; }
 
+		[Required(ErrorMessage = "Este Campo es Requerido.")]
+		[StringLength(11,ErrorMessage = "Limite excedido.")]
+		[RegularExpression(@"^[0-9]+$", ErrorMessage = "Solo Caracteres Alfabeticos.")]
 		public string Rnc { get; set; }
 
+		[Required(ErrorMessage = "Este Campo es Requerido.")]
+		[Range(1, double.MaxValue, ErrorMessage = "Solo Caracteres Numericos.")]
 		public double LimiteCredito { get; set; }
 
 		[ForeignKey("TecnicoId")]
