0b8396b5df88afbada4cd63f06b6ce1afb7330e2
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-01-25 03:16:39 -0400

    Segunda Migracion.

---
+++ Registro/Migrations/20250125070315_Secondary.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Registro.Migrations
+{
+    /// <inheritdoc />
+    public partial class Secondary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tecnicos",
+                columns: table => new
+                {
+                    TecnicoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SueldoHora = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tecnicos", x => x.TecnicoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaIngreso = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rnc = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    LimiteCredito = table.Column<double>(type: "float", nullable: false),
+                    TecnicoId = table.Column<int>(type: "int", nullable: false),
+                    TecnicosTecnicoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ClienteId);
+                    table.ForeignKey(
+                        name: "FK_Clientes_Tecnicos_TecnicosTecnicoId",
+                        column: x => x.TecnicosTecnicoId,
+                        principalTable: "Tecnicos",
+                        principalColumn: "TecnicoId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_TecnicosTecnicoId",
+                table: "Clientes",
+                column: "TecnicosTecnicoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Tecnicos");
+        }
+    }
+}
