e5fbe35f419ef9d4a89e65848bdb8925338b37cb
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-01-25 02:10:00 -0400

    Cree el modelo clientes.

---
+++ Registro/Models/Clientes.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Registro.Models
+{
+	public class Clientes
+	{
+		[Key]
+		public int ClienteId { get; set; }
+
+		public DateTime FechaIngreso { get; set; } = DateTime.Now;
+
+		public string Nombres { get; set; }
+
+		public string Direccion { get; set; }
+
+		public string Rnc { get; set; }
+
+		public double LimiteCredito { get; set; }
+
+		[ForeignKey("TecnicoId")]
+		public int TecnicoId { get; set; }
+		public Tecnicos? Tecnicos { get; set; }
+	}
+}
