159b30343b81f53b3a2f93b0ad7431fbb89e8422
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-01-27 20:26:08 -0500

    Se creo el modelo ciudades.

---
+++ Registro/Models/Ciudades.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registro.Models
+{
+	public class Ciudades
+	{
+		[Key]
+		public int CiudadId { get; set; }
+
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		[RegularExpression(@"^[a-zA-Z\s]+$",ErrorMessage = "Solo Caracteres alfabeticos.")]
+		public string? Nombre { get; set; }
+	}
+}
