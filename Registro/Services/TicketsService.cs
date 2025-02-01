d863db6ffa635751c787a7c88c3b8afa4037edfc
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-02-01 00:04:50 -0400

    Hice el Servicio de Tickets.

---
+++ Registro/Services/TicketsService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro.DAL;
+using Registro.Models;
+using System.Linq.Expressions;
+
+namespace Registro.Services
+{
+	public class TicketsService(IDbContextFactory<Contexto> DbFactory)
+	{
+		public async Task<bool> Guardar(Tickets ticket)
+		{
+			if (!await Existe(ticket.TicketId))
+				return await Insertar(ticket);
+			else
+				return await Modificar(ticket);
+		}
+
+		private async Task<bool> Modificar(Tickets ticket)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			contexto.Update(ticket);
+			return await contexto.SaveChangesAsync() > 0;
+		}
+
+		private async Task<bool> Insertar(Tickets ticket)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			contexto.Tickets.Add(ticket);
+			return await contexto.SaveChangesAsync() > 0;
+		}
+
+		private async Task<bool> Existe(int ticketId)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			return await contexto.Tickets
+				.AnyAsync(t => t.TicketId == ticketId);
+		}
+
+		public async Task<bool> Eliminar(int ticketId)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			return await contexto.Tickets
+				.Where(t => t.TicketId == ticketId)
+				.ExecuteDeleteAsync() > 0;
+		}
+
+		public async Task<Tickets?> Buscar(int ticketId)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			return await contexto.Tickets
+				.FirstOrDefaultAsync(t => t.TicketId == ticketId);
+		}
+		
+		public async Task<List<Tickets>> Listar(Expression<Func<Tickets,bool>> criterio)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			return await contexto.Tickets
+				.Include(t => t.Tecnicos)
+				.Where(criterio)
+				.ToListAsync();
+		}
+	}
+}
