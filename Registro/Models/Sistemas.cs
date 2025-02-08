cd51c6188776c5ae8648f3d771a837dba217de6b
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-02-08 18:26:42 -0400

    Modelo Sistemas Creado.

---
+++ Registro/Models/Sistemas.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registro.Models
+{
+	public class Sistemas
+	{
+		[Key]
+		public int SistemaId { get; set; }
+
+		[Required(ErrorMessage = "Este Campo es Requerido.")]
+		public string Descripcion { get; set; }
+
+		[Required(ErrorMessage = "Este Campo es Requerido.")]
+		[Range(1, 100, ErrorMessage = "Solo Caracteres Numericos del 1 al 100.")]
+		public decimal Complejidad { get; set; }
+	}
+}
